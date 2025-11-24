6bf6ca9c36231d3c34fe1b5d1265517cf293f068
zach-planet-nine <zach@planetnine.app>
2025-11-24 14:42:38 -0800

    Add missing index.js to wiki-plugin-allyabase
    - Wiki plugins require index.js as entry point
    - Exports server module for server-side route registration
    - This enables proxy routes to actually load

---
+++ src/wiki/wiki-plugin-allyabase/index.js
@@ -0,0 +1,6 @@
+// Main entry point for wiki-plugin-allyabase
+// This file is required by federated wiki to load the server-side plugin
+
+module.exports = {
+  server: require('./server/server.js')
+};
